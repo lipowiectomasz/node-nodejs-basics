667ef5c92f991333910e3bba3f60db89355d6d47
Tomasz Lipowiec <t.lipowiec@lemonmind.com>
2025-04-28 23:11:15 +0200

    feat: hash script implementation

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import {dirname, join} from 'path';
+import {fileURLToPath} from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+    const hash = createHash('sha256');
+    const stream = createReadStream(filePath);
+
+    stream.on('data', (chunk) => hash.update(chunk));
+    stream.on('end', () => {
+        console.log(hash.digest('hex'));
+    });
 };
 
 await calculateHash();
