b7d6f987981a9d54c4c892f9a7642ad9a76e67ef
Tomasz Lipowiec <t.lipowiec@lemonmind.com>
2025-04-28 23:12:51 +0200

    feat: wt scripts implementations

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,35 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+import {dirname, join} from 'path';
+import {fileURLToPath} from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const performCalculations = async () => {
-    // Write your code here
+    const numCPUs = os.cpus().length;
+    const workers = [];
+
+    const createWorker = (workerData) => {
+        return new Promise((resolve) => {
+            const worker = new Worker(join(__dirname, 'worker.js'), { workerData });
+
+            worker.on('message', (data) => {
+                resolve({ status: 'resolved', data });
+            });
+
+            worker.on('error', () => {
+                resolve({ status: 'error', data: null });
+            });
+        });
+    };
+
+    for (let i = 0; i < numCPUs; i++) {
+        workers.push(createWorker(10 + i));
+    }
+
+    const settledResults = await Promise.all(workers);
+    console.log(settledResults);
 };
 
 await performCalculations();
