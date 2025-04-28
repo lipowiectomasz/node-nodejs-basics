ccf86ca872144b0c01fad885fedbc8a91cab8237
Tomasz Lipowiec <t.lipowiec@lemonmind.com>
2025-04-28 23:08:24 +0200

    feat: cp script implementation

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,14 @@
+import { spawn } from 'child_process';
+import { join } from 'path';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = spawn('node', [join('src', 'cp', 'files', 'script.js'), ...args], {
+        stdio: ['pipe', 'pipe', 'inherit']
+    });
+
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['arg1', 'arg2']);
