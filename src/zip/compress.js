af5d46c07c03a943fa4fbcb723cc29c4839b8b97
Tomasz Lipowiec <t.lipowiec@lemonmind.com>
2025-04-28 23:13:05 +0200

    feat: zip scripts implementations

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import { createReadStream, createWriteStream } from 'fs';
+import {dirname, join} from 'path';
+import { createGzip } from 'zlib';
+import {fileURLToPath} from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+    const source = join(__dirname, 'files', 'fileToCompress.txt');
+    const destination = join(__dirname, 'files', 'archive.gz');
+
+    const readable = createReadStream(source);
+    const writable = createWriteStream(destination);
+    const gzip = createGzip();
+
+    readable.pipe(gzip).pipe(writable);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
+import { createReadStream, createWriteStream } from 'fs';
+import {dirname, join} from 'path';
+import { createUnzip } from 'zlib';
+import {fileURLToPath} from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+    const source = join(__dirname, 'files', 'archive.gz');
+    const destination = join(__dirname, 'files', 'fileToCompress.txt');
+
+    const readable = createReadStream(source);
+    const writable = createWriteStream(destination);
+    const unzip = createUnzip();
+
+    readable.pipe(unzip).pipe(writable);
 };
 
 await decompress();
